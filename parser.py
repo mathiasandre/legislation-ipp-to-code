e14ef8a34f54f32019e82ed0566924375d00a2b5
Guillot <malkaguillot@gmail.com>
2014-04-07 18:27:04 +0200

    A fine parser with some todo:
    Works with 'bareme - Prestations.xls' (there is work to do on the other
    xls files)
    Pb : handle accents in the file and sheet imports

--- parser.py
+++ parser.py
@@ -2,50 +2,56 @@
 
 import numpy as np
 import pandas as pd
-import datetime
-
+import math
+import os
 # Architecture : 
-# un xlsx contient des sheets qui contiennent des variables, chaque sheet ayant un vecteur de dates/
+# un xlsx contient des sheets qui contiennent des variables, chaque sheet ayant un vecteur de dates
 
-def import_xls(param_name):# Travail sur Prestations uniquement pour l'instant
-    return pd.ExcelFile('C:/Users/m.guillot/My Documents/Aptana Studio 3 Workspace/Param_IPP/Baremes IPP - '+param_name+'.xlsx')
-
+def import_xls(param_name):
+    path = os.path.dirname(__file__)
+    return pd.ExcelFile(path+'/Baremes IPP - '+param_name+'.xlsx')
+    
 def clean_sheet(sheet_name):
     ''' Cleaning excel sheets and creating small database'''
+
     sheet = xlsxfile.parse(sheet_name, index_col = None)
-    #print sheet.columns.values # Column names
-    print sheet.columns.values
-    for col in sheet.columns.values:     #Conserver les bonnes colonnes : on drop tous les "Unnamed"
+
+    # Conserver les bonnes colonnes : on drop tous les "Unnamed"
+    for col in sheet.columns.values:     
         if (col[0:7] == 'Unnamed'):
             sheet = sheet.drop([col],axis = 1)
-
-    #Conserver les bonnes lignes : on drop une ou deux lignes du début
-    #TODO: utiliser cette ligne pour faire des labels/descriptions
-    list_prestation = ['PAJE_CM', 'ALF1', 'ALF4', 'ALF6', 'ALF8', 'ALF9', 'ALF12'] 
-    if sheet_name in list_prestation: #TODO: eventuellement améliorer en testant si la cellule d' est un nombre et delete sinon
-        sheet = sheet.ix[2:]  
-    else: 
-        sheet = sheet.ix[1:]
-    #TODO: droper les lignes de la fin quand il n'y a pas de date mais des trucs écrits
-
-    # Handle dates
-
-    # Handle currencies
-    def _francs_to_euros(var):
-        ''' Fonction qui converti les francs en euro'''
-        sheet[var][pd.DatetimeIndex(sheet['date']).year <= 2001] = np.divide(sheet[var][pd.DatetimeIndex(sheet['date']).year <= 2001],6.55957) # tout est en euro du coup
-        return sheet[var]
-    #TODO: utiliser la fonction uniquement quand on a un montant nominal (mais pas pour les taux ou les conditions d'ages par ex.) 
-        # => comment savoir s'il est nominal ou non ? (idée : tester le résultat est proche de l'année d'après (par exemple, maximum 10% de variation ?)
-        # sinon, on fait la liste à la main
+   
+    def _is_var_nan(row,col):
+        fusion = False
+        if isinstance(sheet.iloc[row,col],float):
+            fusion = math.isnan(sheet.iloc[row,col])
+        return fusion
+    
+    # Conserver les bonnes lignes : on drop s'il y a du texte ou du NaN dans la colonne des dates
+    sheet['date_renseignees'] = False
+    for i in range(0,sheet.shape[0]):
+        sheet.loc[i,['date_renseignees']] = isinstance(sheet.iat[i,0],unicode) | _is_var_nan(i,0)
+    sheet = sheet[sheet.date_renseignees == False]
+    sheet = sheet.drop(['date_renseignees'],axis = 1)
+    
+    # S'il y a du texte au milieu du tableau (explications par exemple) => on le transforme en NaN
+    for col in range(0,sheet.shape[1]):
+        for row in range(0,sheet.shape[0]):
+            if isinstance(sheet.iloc[row,col],unicode):
+                sheet.iat[row,col] = 'NaN'
+    # TODO: Handle currencies (Pb : on veut ne veut diviser que les montants et valeurs monétaires mais pas les taux ou conditions).
+    # TODO: Utiliser les lignes supprimées du début pour en faire des lables
+    # TODO: Utiliser les lignes supprimées de la fin et de la droite donner des informations sur la législation (références, notes...)
     return sheet
     
     
 if __name__ == '__main__':
 
     xlsxfile =   import_xls('Prestations')
-    sheet_names = xlsxfile.sheet_names # get all sheet names
-
-    bmaf = clean_sheet('BMAF')
-
-
+    sheet_names = xlsxfile.sheet_names
+#    list_prestation = ['PAJE_CM', 'ALF1', 'ALF4', 'ALF6', 'ALF8', 'ALF9', 'ALF12'] La liste des cas compliqués pour Prestation
+    sheet_names = [ v for v in sheet_names if not v.startswith('Sommaire')|v.startswith('Outline') | v.startswith('Barème IGR')]
+    print sheet_names
+    dic={}
+    for sheet in  sheet_names:
+        dic[sheet]= clean_sheet(sheet)
